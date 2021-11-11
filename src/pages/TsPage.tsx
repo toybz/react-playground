f18cb7bc26467370afea3aa4ec5734e9deca8699
Toyeeb <toyeeb@flutterwavego.com>
2021-11-11 15:08:36 +0100

    update

--- src/pages/TsPage.tsx
+++ src/pages/TsPage.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export function TypeScriptPage() {
+type Props = {
+  pageTitle?: string;
+  pageBackground?: string;
+  key?: number;
+  isVisible?: boolean;
+  items?: string[];
+};
+
+export function TypeScriptPage(props: Props) {
+  const [userName, setUsername] = useState<string>("");
+
+  const updateUsername = () => {
+    setUsername("Set");
+  };
+
+  useEffect(() => {
+    updateUsername();
+  }, []);
+
   return (
     <>
       <p> Typescript Page djbdihddi</p>
+      <p>Username is {userName}</p>
     </>
   );
 }
